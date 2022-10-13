2ef965c825e9da6f7a72c3595174bb6ac8f8cd2d
Kals <cherivov@mail.ru>
2022-10-13 21:22:30 +0300

    task01

--- task01.py
+++ task01.py
@@ -1,18 +1,7 @@
-a = int(input('Введите день: '))
-b = int(input('Введите месяц: '))
-if (a>0 and a<8) and (b>0 and b<13):
-    if a == 1 and b == 1:
-        print('Новый год')
-    elif a ==2 and b==2:
-        print('День Защитника отечества')
-    elif a ==1 and b==3:
-        print('Международный женский день')
-    elif a==2 and b==5:
-        print('День победы')
-    else:
-        if 0 < a and a < 6:
-            print('не выходной')
-        elif 5 < a and a < 8:
-            print('выходной')
+a = int(input('Введите день недели: '))
+if 0 < a and a < 6:
+    print('не выходной')
+elif 5 < a and a < 8:
+    print('выходной')
 else:
     print('Я вас не понимаю')
